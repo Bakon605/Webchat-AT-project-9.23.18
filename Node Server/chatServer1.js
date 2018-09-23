639b1b3bf7db5e34d06ed6937187d8943182fa41
Bakon605 <42582877+Bakon605@users.noreply.github.com>
2018-09-23 15:22:49 -0500

    Initialization
    added packages for express and socket.io. I attempted to move the test script onto the new interface, but there is an issue communicating to the server application

---
+++ Node Server/chatServer1.js
@@ -0,0 +1,31 @@
+var exp = require('express')();
+var http = require('http').Server(exp);
+var io = require('socket.io')(http);
+
+exp.get('/', function(req, res) {
+
+	res.sendFile(__dirname + '/interface/interface1.html');
+	
+});
+
+io.on('connection', function(socket){
+	
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+		console.log('user left');
+		
+		
+	});
+	
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+	
+
+	
+});
+
+
+http.listen(8069, function(){
+	console.log('listening on 8069');
+});
